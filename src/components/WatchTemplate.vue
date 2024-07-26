c92a306fc5708b96deb07a04de958f77ebd712e1
innTall <avg999.trade@gmail.com>
2024-07-26 11:39:19 +0200

    build: create search and select options

--- src/components/WatchTemplate.vue
+++ src/components/WatchTemplate.vue
@@ -1,9 +1,33 @@
 <script setup>
-import { ref } from 'vue';
+import DOMPurify from 'dompurify';
+import { ref, computed } from 'vue';
 import { storeToRefs } from "pinia";
 import { useTickerStore } from '../stores/tickers.js';
 const { tickers } = storeToRefs(useTickerStore());
 const searchTicker = ref('');
+const selectedTicker = ref('');
+const searchTickers = computed(() => {
+	if (searchTicker.value === "") return [];
+	const searchPattern = new RegExp(`^${searchTicker.value}`, "i");
+	let matches = 0;
+	return tickers.value.filter((ticker) => {
+		if (searchPattern.test(ticker.symbol) && matches < 25) {
+			matches++;
+			return ticker;
+		}
+	});
+});
+const highlightMatch = (tickerSymbol) => {
+	const regex = new RegExp(`(^${searchTicker.value})`, "i");
+	const highlighted = tickerSymbol.replace(
+		regex, `<span id="highlight" class="text-red-600">$1</span>`
+	);
+	return DOMPurify.sanitize(highlighted);
+};
+const selectTicker = (ticker) => {
+	selectedTicker.value = ticker;
+	searchTicker.value = '';
+};
 </script>
 
 <template>
@@ -11,6 +35,15 @@ const searchTicker = ref('');
 		<label for="search">Tickers</label>
 		<div>
 			<input v-model="searchTicker" type="text" id="search" placeholder="Type here...">
+		</div>
+		<div>
+			<ul v-if="searchTickers.length">
+				<li> Showing {{ searchTickers.length }} of {{ tickers.length }} results</li>
+				<li v-for="ticker in searchTickers" :key="ticker.symbol" @click="selectTicker(ticker.symbol)"
+					v-html="highlightMatch(ticker.symbol)">
+				</li>
+			</ul>
+			<p v-if="selectedTicker"> You have selected: {{ selectedTicker }} </p>
 		</div>
 	</div>
 </template>
